e79b4b58018de80d29d4176a72db4dfafcd2e009
Maria Julia Pensotti <julia.pensotti@gmail.com>
2022-08-15 18:44:33 -0300

    cors

--- src/main/java/com/portfolioMJP/mjp/Security/Controller/AuthController.java
+++ src/main/java/com/portfolioMJP/mjp/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/")
-@CrossOrigin(origins = "https://portfoliomjp-40113.web.app/auth")
+@CrossOrigin
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
