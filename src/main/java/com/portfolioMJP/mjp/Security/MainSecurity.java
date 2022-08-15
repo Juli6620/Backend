1ff11c671929711cbc956d000e36507b60459bad
Maria Julia Pensotti <julia.pensotti@gmail.com>
2022-08-15 16:31:26 -0300

    cors

--- src/main/java/com/portfolioMJP/mjp/Security/MainSecurity.java
+++ src/main/java/com/portfolioMJP/mjp/Security/MainSecurity.java
@@ -44,6 +44,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("**").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
