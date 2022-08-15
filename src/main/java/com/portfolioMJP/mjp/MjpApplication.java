13929e33011c4a44fcfc802edd70f32f285272da
Maria Julia Pensotti <julia.pensotti@gmail.com>
2022-08-15 17:31:49 -0300

    cors

--- src/main/java/com/portfolioMJP/mjp/MjpApplication.java
+++ src/main/java/com/portfolioMJP/mjp/MjpApplication.java
@@ -2,13 +2,30 @@ package com.portfolioMJP.mjp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MjpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MjpApplication.class, args);
-	}
+
+     public static void main(String[] args) {
+        SpringApplication.run(MjpApplication.class, args);
+    }
+
+     @Bean
+        public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**")
+               .allowedOrigins("*")
+               .allowedMethods("*")
+               .allowedHeaders("*");
+           }
+        };
+    }
 
 }
- 
+        
